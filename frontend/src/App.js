5bab751e409bfed02bb942c295e007e9a715a62d
Zacharia Beshir <zachariabeshir2@gmail.com>
2025-06-21 15:19:39 -0700

    Initial project setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <h1>Yo</h1>
+    </>
+  );
+}
+
+export default App;
